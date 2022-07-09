eec3f198374dbe6eb4481d2c1728ad519f659697
Emiliano <emilianohaag10@gmail.com>
2022-07-09 13:14:14 -0300

    Excersice resolved of hacker rank

---
+++ recursiveDigitSum.py
@@ -0,0 +1,12 @@
+
+
+
+def superDigit(n, k):
+    if len(n) == 1 and k == 1:
+        return n
+    total = str(sum([int(d) for d in n]) * k)
+    return superDigit(total, 1)
+   
+
+
+print(superDigit('9875',4))
